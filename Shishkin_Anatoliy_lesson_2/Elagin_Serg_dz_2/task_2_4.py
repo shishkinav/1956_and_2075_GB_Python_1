b5301ac987af900a9936bbb42767f7ef058ca87f
fxshad <fxshad@mail.ru>
2022-01-30 03:27:24 +0700

    ready task4

---
+++ Shishkin_Anatoliy_lesson_2/Elagin_Serg_dz_2/task_2_4.py
@@ -0,0 +1,35 @@
+def convert_name_extract(list_in: list) -> list:
+    """Извлекает имена из элементов и формирует список приветствий."""
+    # пишите реализацию своей программы здесь
+    # extracting
+    i = 0
+    for element in list_in:
+        cut_name = element.split(' ')
+        list_in[i] = ([cut_name[-1]])
+        i += 1
+    # converting
+    i = 0
+    for element in list_in:
+        convert_name = ''.join(element)
+        list_in.insert(i, 'Привет,  ' + convert_name.capitalize() + '!')
+        del list_in[i + 1]
+        i += 1
+    list_out = list_in       #["здесь должены оказаться результирующие строковые приветствия"]
+    return list_out
+
+
+my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+result = convert_name_extract(my_list)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
