d7acb081001f2a427b54bb1f4ec2954fa90e0f4f
fxshad <fxshad@mail.ru>
2022-01-31 21:03:17 +0700

    tasks_lesson_2_upd

--- Shishkin_Anatoliy_lesson_2/Elagin_Serg_dz_2/task_2_5.py
+++ Shishkin_Anatoliy_lesson_2/Elagin_Serg_dz_2/task_2_5.py
@@ -16,7 +16,7 @@ def transfer_list_in_str(list_in: list) -> str:
             float_part = f'0{int(float_part)} коп, '
         else:
             float_part =  f'{int(float_part)} коп, '
-        str_out += int_part +float_part     #"здесь итоговая строка"
+        str_out += int_part + float_part     #"здесь итоговая строка"
     return str_out
 
 
@@ -35,12 +35,13 @@ def sort_prices(list_in: list) -> list:
 
 
 # зафиксируйте здесь информацию по исходному списку my_list
-print('id  my_list:',id(my_list) )
-
-result_2 = sort_prices(my_list)
-# зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом
-
-print('id result_2:',id(result_2) )
+print('id  my_list:',id(my_list) )    #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#                                                                                        |
+result_2 = sort_prices(my_list)       #                                                 |
+# зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом     | id my_list = id result_2
+#                                                                                       | соответственно это один и тот же обьект
+#                                                                                        |
+print('id result_2:',id(result_2) )    #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 print(result_2,"отсортированный результирующий список")
 
 
@@ -61,17 +62,19 @@ def check_five_max_elements(list_in: list) -> list:
         список из ПЯТИ максимальных значений"""
     # пишите реализацию здесь
     list_out = []
-    sort_prices(list_in)
-    i = -1
-    while i != -6:
-        list_out.append(list_in[i])
-        i -= 1
-
-
-
-
-
-    return  sorted(list_out)
+    _ = [my_list[0]]
+    for elem in my_list:                  # выберает 5 наибольших значений
+        if _[0] < elem:
+            _.insert(0,elem)
+    del _[5:len(_)]
+    # если нельзя использовать sort() то реверс "в ручную"
+    n = len(_)-1
+    while n != -1:
+        _.append(_[n])
+        n -= 1
+    del _[0:int(len(_)/2)]
+    list_out = _
+    return  list_out
 
 
 result_4 = check_five_max_elements(my_list)
